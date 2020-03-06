14b4d4844ec30484852091a41d9d2399d9b198d4
Shivam <patelsd96@gmail.com>
2020-03-06 20:34:56 +0000

    added about routes

--- config/routes.rb
+++ config/routes.rb
@@ -10,6 +10,8 @@ Rails.application.routes.draw do
   resources :products, only: [:index, :show]
   resources :categories, only: [:show]
 
+  # resource :about,  only: [:show]
+
   resource :cart, only: [:show] do
     post   :add_item
     post   :remove_item
@@ -21,6 +23,9 @@ Rails.application.routes.draw do
     root to: 'dashboard#show'
     resources :products, except: [:edit, :update, :show]
   end
+
+  get '/about', to: 'abouts#show'
+
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
