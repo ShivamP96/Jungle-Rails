74533c043e0da845b5a5e53c2634cb67314d5d00
Shivam <patelsd96@gmail.com>
2020-03-10 02:45:00 +0000

    added two colums for first and last name, then deleted name column and then added testing code

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,24 @@
 class User < ActiveRecord::Base
-  
+
   #this line of code gives our User model authentication methods via bcrypt
   has_secure_password
 
+  def self.authenticate_with_credentials(email,password)
+    user = User.find_by_email(email.downcase.strip)
+    if user
+      if user.authenticate(password)
+        return user
+      else 
+        return nil
+      end
+    else
+      return nil
+    end
+  end
+
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+  validates :password, presence: true, length: { minimum: 3 }
+  validates :password_confirmation, presence: true
+  validates :email, presence:true, uniqueness:true
 end
